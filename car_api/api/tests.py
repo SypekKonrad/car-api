734d950484f6b0f48f0f07499bb3741ca34c0017
Konrad Sypek <konrad.sypek@o2.pl>
2024-02-26 15:26:57 +0100

    cars/ post endpoint tests, added status code 400 and 500 case, refactor of status code 200 case

--- car_api/api/tests.py
+++ car_api/api/tests.py
@@ -1,11 +1,41 @@
-from django.test import TestCase
 from rest_framework.test import APIClient
+from rest_framework import status
+from django.test import TestCase
+from unittest.mock import patch
+from .models import Car
 
-class CarAPITest(TestCase):
+
+class CarsPostTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
 
-#todo  czy napisać test dla modelu którego nie ma w bazie?
-    def test_create_car(self):
+    @patch('requests.get')
+    def test_cars_post_success(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            'Results': [{'Make_Name': 'Toyota', 'Model_Name': 'Prius'}]
+        }
+
         response = self.client.post('/cars/', {'make': 'Toyota', 'model': 'Prius'})
-        self.assertEqual(response.status_code, 201)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Car.objects.filter(make='Toyota', model='Prius').exists())
+
+    @patch('requests.get')
+    def test_cars_post_failure_nonexistent_car(self, mock_get):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {'Results': []}
+
+        response = self.client.post('/cars/', {'make': 'FSO', 'model': 'Polonez'})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse(Car.objects.filter(make='FSO', model='Polonez').exists())
+
+    @patch('requests.get')
+    def test_cars_post_failure_external_api(self, mock_get):
+        mock_get.return_value.status_code = 500
+
+        response = self.client.post('/cars/', {'make': 'Toyota', 'model': 'Prius'})
+
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
