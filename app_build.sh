c52e68144f1aa6acac626b5236cdac106f5269b0
Konrad Sypek <konrad.sypek@o2.pl>
2024-02-28 16:25:24 +0100

    script for app build

---
+++ app_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+cd "$SCRIPT_DIR"
+
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+cd car_api
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver
+#echo "Current directory: $(pwd)"
